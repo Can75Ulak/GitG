cc5795107d86767112245929e62c63ba51eb82d4
Can75Ulak <156481392+Can75Ulak@users.noreply.github.com>
2024-01-13 21:47:30 +0300

    abc

---
+++ src/main/java/com/toker/dunya_ulkeler/DunyaUlkelerApplication.java
@@ -0,0 +1,13 @@
+package com.toker.dunya_ulkeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunyaUlkelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DunyaUlkelerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toker/dunya_ulkeler/DunyaUlkelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toker.dunya_ulkeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunyaUlkelerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
